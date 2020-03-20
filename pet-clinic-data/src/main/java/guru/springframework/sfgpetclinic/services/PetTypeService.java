074809c4a2f514975ab835212192a3c31f5d7c02
Ahmed <ahmad.rizgar.rashid@gmail.com>
2020-03-21 00:55:59 +0100

    done

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+import java.util.Set;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+
+}
