5eacf6b1c2a1ce09845044ab40866aca9fd2fe65
Ahmed <ahmad.rizgar.rashid@gmail.com>
2020-03-28 23:59:16 +0100

    done

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,13 +1,23 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.security.PublicKey;
 import java.util.Set;
 
 //commit
+@Entity
+@Table(name = "owners") // setup owner table as owners in db.
 public class Owner extends Person {
+    @Column(name = "address")
     public String address;
+
+    @Column(name = "city")
     public String city;
+
+    @Column(name = "telephone")
     public String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets;
 
     public String getAddress() {
