a91d4eaf9f26616390cf3b7954af2c5aadf04d4a
Ahmed <ahmad.rizgar.rashid@gmail.com>
2020-03-29 15:56:50 +0200

    done

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -4,6 +4,6 @@ import guru.springframework.sfgpetclinic.model.Owner;
 
 
 public interface OwnerService extends CrudService<Owner, Long> {
-    Owner findById(long id);
+    Owner findByLastName(String lastName);
 
 }
