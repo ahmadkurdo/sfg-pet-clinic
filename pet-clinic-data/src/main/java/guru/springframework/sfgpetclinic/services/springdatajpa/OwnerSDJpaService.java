d9ba13a72a0b3be5482a37f5e1e4ba1d8346738f
ahmadkurdo <ahmad.rizgar.rashid@gmail.com>
2020-04-06 23:21:38 +0200

    done

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -26,6 +26,7 @@ public class OwnerSDJpaService implements OwnerService {
     public Owner findByLastName(String lastName) {
         return ownerRepository.findByLastName(lastName);
 
+
     }
 
     @Override
